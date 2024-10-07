493bb93fadd072dad1b1a10a9b062a07d826b1bd
Роман Беспалов <bespalovroma1995@gmail.com>
2024-10-07 12:25:08 +0300

    update models

--- backend/competencies/admin.py
+++ backend/competencies/admin.py
@@ -1,27 +1,10 @@
 from django.contrib import admin
 
-from .models import Skills, EmployeeSkills, Evaluation, IndividualDevelopmentPlan
+from .models import (
+    Skills, EmployeeSkills, Evaluation, IndividualDevelopmentPlan
+)
 
 admin.site.register(Skills)
 admin.site.register(EmployeeSkills)
 admin.site.register(Evaluation)
 admin.site.register(IndividualDevelopmentPlan)
-
-
-
-
-# from .models import Competence, CompetenceUser, CompetencyAssessment, CompetencyAssessmentUser
-
-# admin.site.register(Competence)
-# admin.site.register(CompetencyAssessmentUser)
-
-# @admin.register(CompetenceUser)
-# class CompetenceUserAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'user',
-#         'competence',
-#         'date_passage'
-#     )
-
-
-# admin.site.register(CompetencyAssessment)
