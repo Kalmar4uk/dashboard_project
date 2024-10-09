df56be7ceeb7f7ce7550088c1b74a64191ccf30f
Роман Беспалов <bespalovroma1995@gmail.com>
2024-10-09 22:50:48 +0300

    new commit

--- backend/competencies/models.py
+++ backend/competencies/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from .constants import SKILLS
 # import uuid
 # from users.models import Team, Employee
 
